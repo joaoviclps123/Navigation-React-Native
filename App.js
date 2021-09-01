ad2e44844862ace40ef8dd9c5f865b6e0a73bed6
João Victor <44348486+joaoviclps123@users.noreply.github.com>
2021-09-01 18:27:04 -0300

    App.js Atualizado e finalizado

--- App.js
+++ App.js
@@ -1,9 +1,54 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import RelatorioCovid from './components/ListaDados';
+import RegistroDeAtendimento from './components/Detalhes';
 
-import Navegacao from './components/Navegacao';
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <Navegacao />
+    <>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="RelatorioCovid">
+          <Stack.Screen
+            name="RelatorioCovid"
+            component={RelatorioCovid}
+            options = {{
+              title: "Relatorio COVID-19",
+              headerStyle: {
+                backgroundColor: 'blue',
+                textAlign: 'left'
+              },
+              headerTintColor: '#fff',
+              headerTitleStyle: {
+                fontWeight: 'bold',
+                fontSize: 20,
+              }
+            }}
+          />
+          <Stack.Screen
+            name="RegistroDeAtendimento"
+            component={RegistroDeAtendimento}
+            options = {{
+              title: "Registro de Atendimento",
+              headerStyle: {
+                backgroundColor: 'blue',
+                textAlign: 'left'
+              },
+              headerTintColor: '#fff',
+              headerTitleStyle: {
+                fontWeight: 'bold',
+                fontSize: 20,
+              }
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+      <StatusBar style="auto" />
+    </>
   );
-};
+}
+
