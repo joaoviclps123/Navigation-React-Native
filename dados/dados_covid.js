b93a83fdf6a517b0039c1043ea5a74d0b9f1ff54
João Victor <44348486+joaoviclps123@users.noreply.github.com>
2021-09-01 18:40:17 -0300

    base em JS de onde puxei as informações

---
+++ dados/dados_covid.js
@@ -0,0 +1,154 @@
+import { v4 as uuid } from 'uuid';
+
+export const DADOS_COVID = [
+    {
+        id: uuid(),
+        nome: "Mario de Andrade",
+        sexo: "masculino",
+        data_nasc: "25/04/1986",
+        data_atendimento: "10/03/2020",
+        sintomas: "febre, dor de cabeça, coriza",
+        doenca_preexitente: "não há",
+        statuscovid: "suspeito"
+    },
+    {
+        id: uuid(),
+        nome: "Luciana de Almeida Prado",
+        sexo: "feminino",
+        data_nasc: "13/01/1990",
+        data_atendimento: "10/03/2020",
+        sintomas: "febre, falta de ar",
+        doenca_preexitente: "diabetes",
+        statuscovid: "negativo"
+    },
+    {
+        id: uuid(),
+        nome: "Roberto Cruzoé",
+        sexo: "masculino",
+        data_nasc: "01/04/1960",
+        data_atendimento: "13/03/2020",
+        sintomas: "febre, dor de cabeça, coriza, falta de ar",
+        doenca_preexitente: "não há",
+        statuscovid: "positivo"
+    },
+    {
+        id: uuid(),
+        nome: "Mauricio Boro",
+        sexo: "masculino",
+        data_nasc: "25/04/1999",
+        data_atendimento: "20/03/2020",
+        sintomas: "febre, dor de cabeça, coriza",
+        doenca_preexitente: "não há",
+        statuscovid: "suspeito"
+    },
+    {
+        id: uuid(),
+        nome: "Mariana Castro",
+        sexo: "feminino",
+        data_nasc: "14/06/1983",
+        data_atendimento: "30/03/2020",
+        sintomas: "nauseas, diarreia, dor de cabeça",
+        doenca_preexitente: "asma",
+        statuscovid: "negativo"
+    },
+    {
+        id: uuid(),
+        nome: "Julia de Souza",
+        sexo: "feminino",
+        data_nasc: "13/05/1997",
+        data_atendimento: "01/04/2020",
+        sintomas: "febre, falta de ar",
+        doenca_preexitente: "não há",
+        statuscovid: "positivo"
+    },
+    {
+        id: uuid(),
+        nome: "Gustavo Calixto",
+        sexo: "masculino",
+        data_nasc: "03/12/1982",
+        data_atendimento: "02/04/2020",
+        sintomas: "falta de ar, coriza, febre",
+        doenca_preexitente: "não há",
+        statuscovid: "suspeito"
+    },
+    {
+        id: uuid(),
+        nome: "Julieta Severo",
+        sexo: "feminino",
+        data_nasc: "30/12/1987",
+        data_atendimento: "10/04/2020",
+        sintomas: "febre, falta de ar, nauseas",
+        doenca_preexitente: "hipertensao",
+        statuscovid: "positivo"
+    },
+    {
+        id: uuid(),
+        nome: "José Celestino Carrete",
+        sexo: "masculino",
+        data_nasc: "01/03/1955",
+        data_atendimento: "20/04/2020",
+        sintomas: "febre, dor de cabeça, coriza, nauseas",
+        doenca_preexitente: "não há",
+        statuscovid: "positivo"
+    },
+    {
+        id: uuid(),
+        nome: "Joaquina Ferreira da Silva",
+        sexo: "feminino",
+        data_nasc: "30/05/1980",
+        data_atendimento: "20/04/2020",
+        sintomas: "coriza, falta de ar",
+        doenca_preexitente: "não há",
+        statuscovid: "positivo"
+    },
+    {
+        id: uuid(),
+        nome: "Ermelino Rodrigues de Souza",
+        sexo: "masculino",
+        data_nasc: "03/11/1966",
+        data_atendimento: "25/04/2020",
+        sintomas: "febre, falta de ar",
+        doenca_preexitente: "diabetes",
+        statuscovid: "positivo"
+    },
+    {
+        id: uuid(),
+        nome: "Jéssica Cavalcante",
+        sexo: "feminino",
+        data_nasc: "15/09/1988",
+        data_atendimento: "27/04/2020",
+        sintomas: "dor de cabeça, falta de ar",
+        doenca_preexitente: "hipertensão",
+        statuscovid: "negativo"
+    },
+    {
+        id: uuid(),
+        nome: "Regina Duarte",
+        sexo: "feminino",
+        data_nasc: "25/04/1950",
+        data_atendimento: "30/04/2020",
+        sintomas: "febre, nauseas, diarreia",
+        doenca_preexitente: "não há",
+        statuscovid: "suspeito"
+    },
+    {
+        id: uuid(),
+        nome: "Manoel Bonfim",
+        sexo: "masculino",
+        data_nasc: "15/10/1977",
+        data_atendimento: "01/05/2020",
+        sintomas: "falta de ar, coriza",
+        doenca_preexitente: "asma",
+        statuscovid: "positivo"
+    },
+    {
+        id: uuid(),
+        nome: "Suellen Tamares Souza",
+        sexo: "feminino",
+        data_nasc: "31/03/1944",
+        data_atendimento: "01/05/2020",
+        sintomas: "diarreia, nauseas, falta de ar",
+        doenca_preexitente: "não há",
+        statuscovid: "suspeito"
+    },
+];
